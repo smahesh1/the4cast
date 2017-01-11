00a60bd43d1d91c02397106619034f34cad1d087
Jack Duryea <jackduryea@gmail.com>
2017-01-11 11:27:27 -0600

    .

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -5,6 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject1 import app
+import twilio
 
 @app.route('/')
 @app.route('/home')
