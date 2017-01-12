b1b6f15dd6e1314ed98b5c85a563328c768ee645
Jack Duryea <jackduryea@gmail.com>
2017-01-11 19:21:34 -0600

    .

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -5,7 +5,6 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject1 import app
-import numpy
 
 @app.route('/')
 @app.route('/home')
