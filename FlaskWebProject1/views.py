0e52daddf8e340a90dbe63d7ce3070c4fbee30bb
Jack Duryea <jackduryea@gmail.com>
2017-01-14 12:15:34 -0600

    .

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -5,14 +5,14 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject1 import app
-#import requests
+import requests
 
 #import twilio
 
 @app.route('/')
 @app.route('/home', methods=["POST"])
 def home():
-    #resp = requests.post("https://api.twilio.com/2010-04-01/Accounts/ACe6dfc70070586ef00b1c5a39c6040522/Messages.json", data={"To":"+19707655549","From":"+19709646126","Body":"Hi!"},auth=("ACe6dfc70070586ef00b1c5a39c6040522","2f49cbdc4d91e523accf22158ca269d2"))
+    resp = requests.post("https://api.twilio.com/2010-04-01/Accounts/ACe6dfc70070586ef00b1c5a39c6040522/Messages.json", data={"To":"+19707655549","From":"+19709646126","Body":"Hi!"},auth=("ACe6dfc70070586ef00b1c5a39c6040522","2f49cbdc4d91e523accf22158ca269d2"))
     """Renders the home page."""
     return render_template(
         'index.html',
